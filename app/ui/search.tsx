02449a514747f876d1f3302173a81275a4411f7a
mohrrm <mohrrm@gmail.com>
2026-04-03 20:20:53 -0400

    Added debouncing to ui/search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,18 +8,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
-
-      console.log(`Searching ... ${term}`);
-      
-      const params = new URLSearchParams(searchParams);
-      if (term) {
-        params.set('query', term);
-      } else {
-        params.delete('query');
-      }
-      replace(`${pathname}?${params.toString()}`);
-  }
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching ... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
  
   return (
     <div className="relative flex flex-1 shrink-0">
